bbe43b24f797fbbc47ed277776b36701b5f515af
Yannick Bäumer <stix@phcn.de>
2018-01-03 23:13:10 +0100

    added description and license

--- tests/thread_jetbench/jetbench.c
+++ tests/thread_jetbench/jetbench.c
@@ -1,19 +1,65 @@
-/*
-  Engine Simulator
-
-  Program to perform turbomachinery design and analysis
-                a)   dry turbojet
-                b)   afterburning turbojet
-                c)   turbofan with separate nozzle
-
-  Written by Dorian Matichard _ July 2009
-
+/**
+ * This tests asses the performance of the scheduler using the
+ * open source jetbench benchmark published in [1] under the following
+ * license:
+ *                             LICENSE
+ * Version 1.0 (beta), 10 December 2009
+ * Copyright (c) 2009-2010, 
+ * School of Computer Science and Electronic Engineering
+ * University of Essex, CO4 3SQ, UK and Ecole Nationale 
+ * d’Electronique, 
+ * Informatique et Radiocommunications de Bordeaux, ENSEIRB 
+ * All rights reserved.
+ * 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * The JetBench Benchmark is licensed as citation-ware. It means that 
+ * you can use JetBench Benchmark freely, but if you publish research
+ * results for which you used JetBench Benchmark, it would be greatly 
+ * appreciated if you would cite the following paper:
+ * 
+ *   [1]   M. Y. Qadri, D. Matichard, and K. D. M. Maier, 
+ *     "JetBench: An Open Source Real-time Multiprocessor Benchmark," 
+ *     in ARCS 2010 - Architecture of Computing Systems, Hannover, 
+ *     Germany, 2010.
+ * 
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the University of Essex, UK or Ecole Nationale 
+ * d’Electronique, Informatique et Radiocommunications de Bordeaux, 
+ * ENSEIRB or the names of its contributors may be used to endorse or 
+ * promote products derived from this software without specific prior 
+ * written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * The benchmark uses a input file with values in the following ranges:
+ * speed, a: a<0 || a>1500
+ * Altitude, b: b<0 || b>50000
+ * Throttle, c: c<45 || c>90
+ * Deadline time, d: d<0
+ *
 */
-
-        // speed, a: a<0 || a>1500
-        // Altitude, b: b<0 || b>50000
-        // Throttle, c: c<45 || c>90
-        // Deadline time, d: d<0
 
 #include<stdlib.h>
 
